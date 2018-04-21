1bc59527c79e805ee1e3c849696071924075bb3b
henriquelandim <32872996+henriquelandim@users.noreply.github.com>
2018-04-20 21:59:35 -0300

    Programa finalizado e comentado!!
    Nessa ultima atualização, foi adicionado um comentário em bloco informando o objetivo do programa.

--- ProgramaAlertaEstoque/AlertaEstoque/Program.cs
+++ ProgramaAlertaEstoque/AlertaEstoque/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/* Este programa foi desnvolvido com intuito de ajudar usuarios que tenham grandes inventários
+ele tem por objetivo alertar o usuario se ele precisa ou não suprir o iventário, tendo três estagios:
+Urgente: 12% ou menos do estoque
+Remediado: de 13% a 20%
+utilizavel: acima de 20%
+*/
+
+
+using System;
 namespace AlertaEstoque
 {
     class Program
@@ -23,9 +31,9 @@ namespace AlertaEstoque
             //com 2 casas decimais. Ao final concatenamos com símbolo de porcentagem
             Console.WriteLine(resultado.ToString("n2")+"%");
             if(resultado <= 12)
-                Console.WriteLine(" *****     Comprar com Urgência!!!     *****");
+                Console.WriteLine(" *****     Supra com Urgência!!!     *****");
             else if(resultado<=20)
-                Console.WriteLine("#### Realizar Compra! ####");
+                Console.WriteLine("#### Remedie o estoque! ####");
             else
                 Console.WriteLine("----     Ponto de Utilização :)  ----");
         }
